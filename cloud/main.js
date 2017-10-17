b3622b2914fd41436cbe5f01870a43b7b2cfd96b
Edwin Kato <edwin.kato@andela.com>
2017-10-17 15:29:55 +0300

    Add master key in push send function

--- cloud/main.js
+++ cloud/main.js
@@ -2,24 +2,30 @@ Parse.Cloud.define('hello', function(req, res) {
     res.success('Hi');
 });
 
+//Push Notification on creation of new Assignment
 Parse.Cloud.afterSave("Assignment", function(request) {
-  var title = request.object.get("title");
-  var description = request.object.get("description");
-  var assignmentId = request.object.id;
-
-  Parse.Push.send({
-    data: {
-        "assignmentId": assignmentId,
-        "title": title,
-        "description": description
-    }
-  }, {
-    success: function() {
-      console.log("Success!");
+    console.log('Parse.serverURL: ' + Parse.serverURL);
+    var title = request.object.get('title');
+    var description = request.object.get('description');
+    var assignmentId = request.object.id;
+    var pushQuery = new Parse.Query(Parse.Installation);
+    Parse.Push.send({
+        where: pushQuery, // Set our Installation query
+        data: {
+            "assignmentId": assignmentId,
+            "title": title,
+            "description": description
+        }
     },
-    error: function(error) {
-      console.log("There was an error: " + error);
-    }
-  });
+        {
+            useMasterKey: true
+        },
+        {
+        success: function() {
+            console.log("Push notification was sent successfully");
+        },
+        error: function(error) {
+            throw "Got an error " + error.code + " : " + error.message;
+        }
+    });
 });
-
