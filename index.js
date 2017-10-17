07cfa9d7c9b6650c0d0d1c5d2b89f440a3bb64c6
Edwin Kato <edwin.kato@andela.com>
2017-10-17 15:28:25 +0300

    Add android push notification configuration

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ var api = new ParseServer({
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  push: {
+    android: {
+      senderId: process.env.SENDER_ID || '', // The Sender ID of GCM
+      apiKey: process.env.API_KEY || '' // The Server API Key of GCM
+    }
+  },
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
